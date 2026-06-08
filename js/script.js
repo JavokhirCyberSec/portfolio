7619e4263d8970a1b1eb2a4d907757ac2957e899
Javokhir Ungorboev <javokhirungarboevGithub@gmail.com>
2026-06-08 13:06:02 +0200

    photos,iconhtbletsd

--- js/script.js
+++ js/script.js
@@ -52,7 +52,7 @@ ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
 
 
 const typed = new Typed('.multiple-text', {
-    strings: ['Cybersecurity Enthusiast','IT-Specialist'],
+    strings: ['a Cybersecurity Enthusiast','an IT-Specialist'],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
